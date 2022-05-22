40b574a7cf96217fb64d6525ecdd6341718ce7d0
smills0e <93284696+smills0e@users.noreply.github.com>
2022-05-23 04:25:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'bbs.eu-gb.cf.appdomain.cloud'
+      url.hostname = 'oklsp-ggrr567.cloud.okteto.net'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
